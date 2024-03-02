47411aef6849ca4921f7a63215dd6dde0955115e
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 23:51:09 +0000

    Update 404 page layout and button width

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,11 +5,11 @@ function NotFound() {
   return (
     <div className="w-full  h-screen flex items-center justify-center flex-col gap-7">
       <img src="/assets/not-found.gif" alt="not-found" />
-      <div className="flex flex-col gap-4 text-center">
+      <div className="flex flex-col gap-4 text-center items-center">
         <h1 className="text-4xl font-bold">Oops!</h1>
         <p className="">Sorry, an unexpected error has occured.</p>
         <p className="text-gray-500 text-sm font-italic">Not Found</p>
-        <Link to="/" className="btn w-[400px]">
+        <Link to="/" className="btn w-[200px]">
           Back to Home
         </Link>
       </div>
