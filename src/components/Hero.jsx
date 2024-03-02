adcc4bc913dfdba257387a820b81b8267c6752ac
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 16:55:39 +0000

    Add Link component to Hero and conditionally render Get Started button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ReactTyped } from "react-typed";
 
-function Hero({ img, headText, subText }) {
+function Hero({ img, headText, subText, page }) {
   return (
-    <div className="px-4 mx-auto max-w-[1240px] flex flex-col lg:flex-row items-center justify-center gap-6">
+    <div
+      className={`px-4 mx-auto max-w-[1240px] ${
+        page === "home" && "lg:mb-10"
+      } flex flex-col lg:flex-row items-center justify-center gap-6`}
+    >
       <div className="text-center mt-8 lg:mt-6 lg:text-left flex flex-col gap-6 md:gap-3 w-[50%]">
         <h1 className="text-3xl lg:text-4xl font-bold">{headText}</h1>
         <p className="text-xl ">{subText}</p>
@@ -22,6 +27,14 @@ function Hero({ img, headText, subText }) {
             loop
           />
         </div>
+        {page !== "quiz" && (
+          <Link
+            to="/quiz"
+            className="btn w-[400px] my-7 flex justify-center items-center lg:py-4 uppercase font-bold text-xl"
+          >
+            Get Started
+          </Link>
+        )}
       </div>
       <div>
         <img src={img} alt={img} />
