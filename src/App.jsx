0ee988d0280b744ef1eb9bf53ee613975e194938
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 14:57:19 +0000

    Add App component and related files

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Layout } from "./components";
+import {
+  About,
+  CreateQuiz,
+  FAQ,
+  Home,
+  NotFound,
+  Quiz,
+  Quizes,
+  Scores,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/scores",
+        element: <Scores />,
+      },
+      {
+        path: "/faq",
+        element: <FAQ />,
+      },
+      {
+        path: "/quiz/create",
+        element: <CreateQuiz />,
+      },
+      {
+        path: "/quiz",
+        element: <Quizes />,
+      },
+      {
+        path: "/quiz/:quizId",
+        element: <Quiz />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <RouterProvider router={router}>
+      <Layout />
+    </RouterProvider>
+  );
+}
+
+export default App;
