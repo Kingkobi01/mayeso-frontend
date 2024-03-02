cf404c2bbb083f3c73d14e4682a4f68c2ad09db8
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-02 16:00:04 +0000

    Update Hero component styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ import { ReactTyped } from "react-typed";
 function Hero({ img, headText, subText }) {
   return (
     <div className="px-4 mx-auto max-w-[1240px] flex flex-col lg:flex-row items-center justify-center gap-6">
-      <div className="text-center mt-8 lg:mt-6 lg:text-left flex flex-col gap-3 w-[50%]">
-        <h1 className="text-5xl lg:text-4xl font-bold">{headText}</h1>
+      <div className="text-center mt-8 lg:mt-6 lg:text-left flex flex-col gap-6 md:gap-3 w-[50%]">
+        <h1 className="text-3xl lg:text-4xl font-bold">{headText}</h1>
         <p className="text-xl ">{subText}</p>
         <div className="font-semibold capitalize text-lg">
           <span>solve questions about </span>
