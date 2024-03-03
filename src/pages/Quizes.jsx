2a23e20328d34dc86b2260597f23ef054561ae5a
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-03 21:39:28 +0000

    Add QuizList component and error handling to Quizes page

--- src/pages/Quizes.jsx
+++ src/pages/Quizes.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { Hero } from "../components";
+import React, { useContext, useState } from "react";
+import { Hero, QuizList } from "../components";
+import QuizContext from "../context/quizContext";
 
 function Quizes() {
+  const { quizes, setQuizes } = useContext(QuizContext);
+
+  const [failed, setfailed] = useState(false);
+
   return (
     <div>
       <Hero
@@ -10,6 +15,17 @@ function Quizes() {
         subText={"Explore Our Collection of Quizzes on Mayeso"}
         page={"quiz"}
       />
+      <div className="py-12">
+        <h1 className="text-5xl text-center">Quizes</h1>
+        {!failed ? (
+          <QuizList quizes={quizes} />
+        ) : (
+          <p className="text-center italic text-xl md:2xl lg:4xl">
+            {" "}
+            Sorry, Something went wrong...
+          </p>
+        )}
+      </div>
     </div>
   );
 }
