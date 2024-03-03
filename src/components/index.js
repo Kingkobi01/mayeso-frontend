65dfc0dd2ec06f2f5d1cda6def3e63c8e0dd6a9c
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-03 21:37:01 +0000

    Add new components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,9 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import Layout from "./Layout";
 import Navbar from "./Navbar";
+import Question from "./Question";
+import QuizCard from "./QuizCard";
+import QuizList from "./QuizList";
 
-export { Footer, Hero, Layout, Navbar };
+export { Footer, Hero, Layout, Navbar, Question, QuizCard, QuizList };
 
