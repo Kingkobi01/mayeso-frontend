b90368f39a06904e7227aec6a79208507e1d3c4a
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-04 17:44:37 +0000

    Update TeamMember

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -3,7 +3,7 @@ import { FaGithubSquare, FaLinkedinIn, FaTwitterSquare } from "react-icons/fa";
 
 function TeamMember(member) {
   return (
-    <div class="max-w-md rounded overflow-hidden shadow-gray-600 shadow bg-secondary hover:shadow-lg hover:shadow-gray-600 duration-300 cursor-pointer">
+    <div class="w-full lg:max-w-md rounded overflow-hidden shadow-gray-600 shadow bg-secondary hover:shadow-lg hover:shadow-gray-600 duration-300 cursor-pointer">
       <img
         class="w-full h-64 object-cover object-center"
         src={member.img}
@@ -15,22 +15,21 @@ function TeamMember(member) {
         <p className="text-sm text-gray-700">{member.place}</p>
       </div>
       <div class="p-2 flex justify-evenly">
-<a target="_blank" href={member.x}>
-
-        <span class="rounded-full inline-block bg-gray-200 p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-          <FaTwitterSquare size={20} />
-        </span>
-</a>
-<a target="_blank" href={member.gitHub}>
-        <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-          <FaGithubSquare size={20} />
-        </span>
-</a>
-<a target="_blank" href={member.linkedIn}>
-        <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-          <FaLinkedinIn size={20} />
-        </span>
-</a>
+        <a target="_blank" href={member.x}>
+          <span class="rounded-full inline-block bg-gray-200 p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+            <FaTwitterSquare size={20} />
+          </span>
+        </a>
+        <a target="_blank" href={member.gitHub}>
+          <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+            <FaGithubSquare size={20} />
+          </span>
+        </a>
+        <a target="_blank" href={member.linkedIn}>
+          <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+            <FaLinkedinIn size={20} />
+          </span>
+        </a>
       </div>
     </div>
   );
