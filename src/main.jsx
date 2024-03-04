43629e4091edcfcbd79e495eaa83647e0c8bb262
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-04 05:26:30 +0000

    Update context providers in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { QuizContextProvider, ScoresContextProvider } from "./context";
+import {
+  QuizContextProvider,
+  QuizScoreContextProvider,
+  ScoresContextProvider,
+} from "./context";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QuizContextProvider>
     <ScoresContextProvider>
-      <App />
+      <QuizScoreContextProvider>
+        <App />
+      </QuizScoreContextProvider>
     </ScoresContextProvider>
   </QuizContextProvider>
 );
