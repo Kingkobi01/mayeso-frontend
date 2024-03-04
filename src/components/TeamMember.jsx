4dbe198c9252702db93d7106fc3d5344286fb23b
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-04 18:03:32 +0000

    Update TeamMember and members.js

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -14,23 +14,25 @@ function TeamMember(member) {
         <p class="text-primary text-base">{member.role}</p>
         <p className="text-sm text-gray-700">{member.place}</p>
       </div>
-      <div class="p-2 flex justify-evenly">
-        <a target="_blank" href={member.x}>
-          <span class="rounded-full inline-block bg-gray-200 p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-            <FaTwitterSquare size={20} />
-          </span>
-        </a>
-        <a target="_blank" href={member.gitHub}>
-          <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-            <FaGithubSquare size={20} />
-          </span>
-        </a>
-        <a target="_blank" href={member.linkedIn}>
-          <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
-            <FaLinkedinIn size={20} />
-          </span>
-        </a>
-      </div>
+      {member.x && member.gitHub && member.linkedIn && (
+        <div class="p-2 flex justify-evenly">
+          <a target="_blank" href={member.x}>
+            <span class="rounded-full inline-block bg-gray-200 p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+              <FaTwitterSquare size={20} />
+            </span>
+          </a>
+          <a target="_blank" href={member.gitHub}>
+            <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+              <FaGithubSquare size={20} />
+            </span>
+          </a>
+          <a target="_blank" href={member.linkedIn}>
+            <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mr-2 mb-2 duration-300 hover:text-white hover:bg-primary">
+              <FaLinkedinIn size={20} />
+            </span>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
--- src/data/members.js
+++ src/data/members.js
@@ -2,7 +2,7 @@ const members = [
     {
         name: "Joshua Attah",
         role: "Backend Developer",
-        place: "Abuja, Nigeria",
+        place: "Kaduna, Nigeria",
         img: "/assets/joshua.jpg",
         x: "https://x.com/that_jogmg",
         gitHub: "https://github.com/JoGMG",
@@ -13,9 +13,9 @@ const members = [
         role: "Backend Developer",
         place: "Malawi",
         img: "/assets/frank.jpg",
-        x: "https://twitter.com/PapaYiadom",
-        gitHub: "https://github.com/Kingkobi01",
-        linkedIn: "https://www.linkedin.com/in/kwabena-boakye-917530197/"
+        x: "",
+        gitHub: "",
+        linkedIn: ""
     },
     {
         name: "Kwabena Boakye",
@@ -29,7 +29,7 @@ const members = [
     {
         name: "Samuel Ifeanyi",
         role: "Frontend Developer",
-        place: "Nigeria",
+        place: "Abuja, Nigeria",
         img: "/assets/sammy.jpg",
         x: "https://twitter.com/OkohSamuelIfea1",
         gitHub: "https://github.com/Dcardinal042",
