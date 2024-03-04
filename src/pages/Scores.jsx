0a1a7a4c0cad656220a835f4511f0116e37fc02c
Kwabena Boakye <kwaboakye93@gmail.com>
2024-03-04 02:08:15 +0000

    Refactor Scores page to display quiz scores from local storage

--- src/pages/Scores.jsx
+++ src/pages/Scores.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import { Hero } from "../components";
 
 function Scores() {
+  const scores = JSON.parse(localStorage.getItem("scores")) || [];
   return (
     <div>
       <Hero
@@ -9,6 +10,32 @@ function Scores() {
         headText={"Track Your Progress, Celebrate Your Success"}
         subText={"View Your Quiz Scores on Mayeso"}
       />
+      <div className="max-w[1240px] px-4 py-16 mx-auto">
+        <h1 className="text-2xl lg:text-3xl my-3 lg:font-semi-bold text-center">
+          Your Scores
+        </h1>
+        <p className="text-xs pt-1 pb-8">
+          Please note that the scores you see here are ones from this browser
+          only
+        </p>
+        {scores &&
+          scores.map((score) => (
+            <div
+              key={score.quizId}
+              className="w-full flex items-center py-4 mb-2 justify-evenly border border-gray-500/50 rounded-md"
+            >
+              <Link
+                to={`/quiz/${score.quizId}`}
+                className="hover:text-secondary duration-150"
+              >
+                {score.title}
+              </Link>
+              <p>
+                {score.score} out of {score.outOf}
+              </p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
